c0df5388e00db1bf4c4512ad70f52052f84fad51
zidankhan86 <zidankhan718@gmail.com>
2023-08-12 03:21:43 +0600

    about by Ajax

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -6,14 +6,66 @@
                 <div class="text-center my-5">
                     <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
                     <p class="lead fw-light mb-4">My name is Start Bootstrap and I help brands grow.</p>
-                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit dolorum itaque qui unde quisquam consequatur autem. Eveniet quasi nobis aliquid cumque officiis sed rem iure ipsa! Praesentium ratione atque dolorem?</p>
+                    <p class="text-muted" id="details">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit dolorum itaque qui unde quisquam consequatur autem. Eveniet quasi nobis aliquid cumque officiis sed rem iure ipsa! Praesentium ratione atque dolorem?</p>
                     <div class="d-flex justify-content-center fs-2 gap-4">
-                        <a class="text-gradient" href="#!"><i class="bi bi-twitter"></i></a>
-                        <a class="text-gradient" href="#!"><i class="bi bi-linkedin"></i></a>
-                        <a class="text-gradient" href="#!"><i class="bi bi-github"></i></a>
+                        <a target="_blank" class="text-gradient" id="tw" href=""><i class="bi bi-twitter"></i></a>
+                        <a target="_blank" class="text-gradient" id="lk" href=""><i class="bi bi-linkedin"></i></a>
+                        <a target="_blank" class="text-gradient" id="gt" href=""><i class="bi bi-github"></i></a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
+
+
+<script>
+
+        About();
+
+    async function About() {
+
+        try {
+
+            let URL = "/aboutData"
+
+            document.getElementById('loading-div').classList.remove('d-none');
+           document.getElementById('content-div').classList.add('d-none');
+
+            let response = await axios.get(URL);
+
+            document.getElementById('details').innerHTML = response.data['details'];
+
+        } catch (error) {
+
+            alert('Error')
+
+        }
+
+    }
+
+    SocialLink();
+    async function SocialLink() {
+
+
+
+        try {
+
+            let URL = "/socialData"
+            let response = await axios.get(URL);
+
+            document.getElementById('loading-div').classList.add('d-none');
+           document.getElementById('content-div').classList.remove('d-none');
+
+            document.getElementById('tw').href = response.data['twitterLink'];
+            document.getElementById('lk').href = response.data['githubLink'];
+            document.getElementById('gt').href = response.data['linkedinLink'];
+
+        } catch (error) {
+
+            alert('Error')
+
+        }
+
+    }
+</script>
